438c7dfa7ed5780fc192a677d3de45601ce9dd2e
Robson Souza <robsonlluis@hotmail.com>
2016-09-28 14:32:01 -0300

    Initial commit

---
+++ app/src/main/java/com/app/rsouza/splash/MainActivity.java
@@ -0,0 +1,22 @@
+package com.app.rsouza.splash;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Reference:
+        //http://tutorialandroid.com.br/como-criar-uma-animacao-para-tela-inicial-do-seu-app/
+    }
+}
---
+++ app/src/main/java/com/app/rsouza/splash/Splashscreen.java
@@ -0,0 +1,51 @@
+package com.app.rsouza.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        StartAnimations();
+    }
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+        LinearLayout l = (LinearLayout) findViewById(R.id.main_layout);
+        if (l != null) {
+            l.clearAnimation();
+            l.startAnimation(anim);
+        }
+
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        if (iv != null) {
+            iv.clearAnimation();
+            iv.startAnimation(anim);
+        }
+
+        int SPLASH_DISPLAY_LENGTH = 3500;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+        /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                Splashscreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
